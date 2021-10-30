48ac848842b2266f6e7434408853bb9e103bdeb4
BINGNAN, CHEN <59923237+r1991108@users.noreply.github.com>
2021-10-30 11:00:40 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,18 +1,56 @@
+// get window size
+let size = document.documentElement.clientWidth;
+console.log(size);
+
+// only when mobile version
+if (size <= 428) {
+  let select = document.querySelector("#answer");
+  let mood = document.querySelector(".mood");
+  let card = document.querySelector(".card");
+  select.addEventListener(
+    "change",
+    (e) => {
+      let colorSelect = e.target.value;
+
+      console.log("test");
+      // console.log(e.target.value);
+      if (colorSelect === "random") {
+        let letters = "0123456789ABCDEF";
+        let colorSelect = "#";
+        for (var i = 0; i < 6; i++) {
+          colorSelect += letters[Math.floor(Math.random() * 16)];
+        }
+        console.log("test2" + colorSelect);
+        card.style["background-color"] = colorSelect;
+        console.log("test3");
+      } else if (colorSelect != "") {
+        card.style["background-color"] = colorSelect;
+      } else {
+        mood.innerHTML = "教えて：）";
+      }
+    },
+    false
+  );
+}
+
+//for desktop
 function changeColor() {
   let mood = document.querySelector(".mood");
   let card = document.querySelector(".card");
-  let option = document.querySelector("#answer");
-  let color = option.value;
-  if (color === "random") {
+  let select = document.querySelector("#answer");
+
+  // answer which is clicked
+  let colorSubmit = select.value;
+  if (colorSubmit === "random") {
     let letters = "0123456789ABCDEF";
-    let color = "#";
+    let colorSubmit = "#";
     for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+      colorSubmit += letters[Math.floor(Math.random() * 16)];
     }
-    card.style["background-color"] = color;
+    card.style["background-color"] = colorSubmit;
   }
-  if (color != "") {
-    card.style["background-color"] = color;
+  if (colorSubmit != "") {
+    card.style["background-color"] = colorSubmit;
   } else {
     mood.innerHTML = "教えて：）";
   }
